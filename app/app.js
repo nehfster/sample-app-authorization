88720171922d135d1bc733913d0368b69919133a
Jim Nehf <nehfster@gmail.com>
2021-04-23 13:09:05 -0700

    fix body-parser deprecation by replacing with express

--- app/app.js
+++ app/app.js
@@ -68,8 +68,8 @@ app.set('view engine', 'pug');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   session({
